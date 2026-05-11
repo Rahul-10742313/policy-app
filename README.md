8b728a7f9ff062098c8c04c85e00a3f85ebe43e8
Rahul-10742313 <rahul.10742313@ltimindtree.com>
2026-05-11 17:18:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# policy-app
+Covermore assessment
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+    return(
+        <div>
+            This is home page.
+        </div>
+    )
+}
+
+export default App;
